0ae8383bfc61c01cefa8ae71a0006fef264afaab
penbosso <ckbosso@st.ug.edu.gh>
2023-06-24 11:00:33 +0300

    [ENC] Rename FK for consistency

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.get('/', (req, res) => {
 
 app.use('/users', require('./router/user-router'));
 app.use('/restaurants', require('./router/restaurant-router'));
+app.use('/menu-items', require('./router/menu-item-router'));
 
 // error handler. 
 app.use(errorHandler);
